ce047cfb5bdc33e8329f84ee4e003a2a3af6ffd8
Murat <murathepeyiler@gmail.com>
2020-10-29 22:26:33 +0300

    refactoring - implementation has been separated

--- inc/Grid.h
+++ inc/Grid.h
@@ -1,63 +1,22 @@
-#ifndef CREATEGRID_H_
-#define CREATEGRID_H_
+#ifndef GRID_H_
+#define GRID_H_
 
+#include <iosfwd>
 #include <vector>
-#include <exception>
-#include <ostream>
-#include <iostream>
 
-class CreateGrid
+class Grid
 {
 public:
-    static CreateGrid* getInstance(size_t sz)
-    {
-        msize = sz;
-        if(!instance)
-            instance = new CreateGrid(sz);
-        return instance;    
-    }
-
-    static CreateGrid* getInstance()
-    {
-        return instance;    
-    }
-
-    static size_t getSize()
-    {
-        return msize;
-    }
-
-    int& operator()(size_t i, size_t j)
-    {
-            return vec.at(i).at(j);
-    }
-
-    friend std::ostream& operator<<(std::ostream&os, CreateGrid* gr)
-    {
-        for(size_t i = 0; i<gr->getSize(); ++i)
-        {
-            for(size_t j = 0; j<gr->getSize(); ++j)
-            {
-                os << gr->operator()(i,j) << ' ';
-            }
-            os << '\n';
-        }
-        return os;
-    }
-
-    static void del(CreateGrid* obj)
-    {
-        delete obj;
-    }
+    static Grid& getInstance(size_t sz = msize);
+    static size_t getSize();
+    int& operator()(size_t i, size_t j);
+    friend std::ostream& operator<<(std::ostream&os, Grid* gr);
 
 private:
-    CreateGrid() = delete;
-    CreateGrid(size_t sz) : vec{std::vector<std::vector<int>>(sz, std::vector<int>(sz,0))} {}
-
+    Grid(size_t sz);
     static inline size_t msize{};
-    static inline CreateGrid* instance{};
-    
     std::vector<std::vector<int>> vec;
+
 };
 
 
---
+++ src/Grid.cpp
@@ -0,0 +1,45 @@
+#include "Grid.h"
+#include <exception>
+#include <ostream>
+#include <iostream>
+
+#define STATIC
+#define PUBLIC
+#define PRIVATE
+#define FRIEND
+
+
+PRIVATE Grid::Grid(size_t sz) : vec{std::vector<std::vector<int>>(sz, std::vector<int>(sz,0))} {}
+
+STATIC PUBLIC Grid& Grid::getInstance(size_t sz)
+{
+    if(sz == 0)
+        throw std::exception{};
+    msize = sz;
+    static Grid gr{sz};
+    return gr;  
+}
+
+STATIC PUBLIC size_t Grid::getSize()
+{
+    return msize;
+}
+
+PUBLIC int& Grid::operator()(size_t i, size_t j)
+{
+    return vec.at(i).at(j);
+}
+
+FRIEND std::ostream& operator<<(std::ostream&os, Grid* gr)
+{
+    for(size_t i = 0; i<gr->getSize(); ++i)
+    {
+        for(size_t j = 0; j<gr->getSize(); ++j)
+        {
+            os << gr->operator()(i,j);
+        }
+        os << '\n';
+    }
+    return os;
+}
+
